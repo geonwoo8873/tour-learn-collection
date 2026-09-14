efd70b3ad21c3d8bb16fa63dda1de149e4b51443
geonwoo desktop <geonwoo8873@outlook.kr>
2026-09-15 00:01:37 +0900

    docs: update IAM security practices with detailed policy explanations and examples

--- python/print.py
+++ python/print.py
@@ -2,8 +2,6 @@
 # Other encodings can be specified if needed cp1252, latin-1, etc.
 # -*- coding: <encoding name> -*-
 
-from typing import Final
-
 ######## Print Output Examples ########
 # Single-line string output using print function
 print("Default encoding for this Python file is utf-8")
@@ -15,14 +13,3 @@ print("""
 
 # `\n` adds a new line before the text outputting to the terminal
 print("\nTest")
-
-######## Variables Declaration ########
-# 
-temperature: Final[int] = -42
-
-update_temperature: Final[float] = temperature
-
-target: int = -20
-
-difference = target-temperature
-print(difference)
